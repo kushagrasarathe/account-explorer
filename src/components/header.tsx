996bad1cb00f787fbe4114b93c370b3bd970e6e2
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2024-12-13 13:51:32 +0530

    fix: update header logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,13 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import { ThemeToggle } from './ui/theme-toggle';
 
 export default function Header() {
   return (
     <div className="z-[1000] flex items-center justify-between pt-6">
-      <Link href={'/'} className={''}>
-        <Image
-          src={'reown.svg'}
-          alt=""
-          width={140}
-          height={100}
-          className="dark:invert"
-        />
+      <Link href={'/'} className="flex gap-1">
+        <span className={'rounded-full bg-black px-4 py-2 text-white'}>
+          gm 🫡
+        </span>
       </Link>
       <div>
         <ThemeToggle />
